77bd1b6557c48a12b81d9385bfcfb6cbe307293d
Brendan Grady <cyhtan@gmail.com>
2016-02-18 22:03:59 -0500

    (refactor) Group properties on recipe model in sub-objects

--- server/api/recipes/recipeModel.js
+++ server/api/recipes/recipeModel.js
@@ -3,14 +3,21 @@ var mongoose = require('mongoose'),
     // hooks    = require('../modelTriggers.js');
 
 var RecipeSchema = new mongoose.Schema({
-    name           : { type: String,   required: true },
-    description    : { type: String },
-    tags           : [],
-    ingredients    : [], // name: _ , amount: _
-    glutenFree     : { type: Boolean },
-    dairyFree      : { type: Boolean },
-    created        : { type: Date },
-    updated        : { type: Date }
+  name        : { type: String,   required: true },
+  description : { type: String },
+  tags        : [],
+  ingredients : [], // name: _ , amount: _ , unit: _
+  mealtime    : {
+                   breakfast: { type: Boolean },
+                   lunch    : { type: Boolean },
+                   dinner   : { type: Boolean }
+                },
+  specialDiet : {
+                   glutenFree : { type: Boolean },
+                   dairyFree  : { type: Boolean }
+                },
+  created     : { type: Date },
+  updated     : { type: Date }
 });
 
 // hooks.setRoadmapHooks(RecipeSchema);
